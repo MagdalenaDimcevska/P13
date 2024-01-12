37c39228ce095c419b4aa62e402380d1c484028d
MagdalenaDimcevska <147504217+MagdalenaDimcevska@users.noreply.github.com>
2024-01-12 16:19:32 +0100

    Add first java project

---
+++ src/Vehiclee.java
@@ -0,0 +1,16 @@
+public class Vehiclee {
+	    private String color;
+
+	    // Constructor with a default color
+	    Vehiclee() {
+	        color = "Blue"; // Set a default color, e.g., "Blue"
+	    }
+
+	    // Constructor with a parameter to set the color
+	    Vehiclee(String a) {
+	        color = a;
+	    }
+	    public String toString() {
+	    return color;
+	}
+}
